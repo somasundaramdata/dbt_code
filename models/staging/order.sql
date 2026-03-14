33902a4ea3f0d3615abed2e8bf48d80ea79d4f16
Somasundaram Nagarajan <somasundaram.data@gmail.com>
2026-03-14 15:46:09 +0000

    commit

--- models/staging/order.sql
+++ models/staging/order.sql
@@ -1,6 +1,7 @@
 {{config(materialized='table')
 }}
 with tbl as (
-    select id 
+    select id,
+           order_date, status  
       from {{source('datafeed_shared_schema','stg_order123')}}
 ) select * from tbl
